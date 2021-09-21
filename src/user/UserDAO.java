f6fe5377b3447417bbe7786c34bbdd32e3ef10ea
SeungSuHa <bonohot95@gmail.com>
2021-09-22 04:29:17 +0900

    회원가입 기능

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -7,42 +7,58 @@ import java.sql.ResultSet;
  
  
 public class UserDAO {
-    
-    private Connection conn;            // DB에 접근하는 객체
-    private PreparedStatement pstmt;    // 
-    private ResultSet rs;                // DB data를 담을 수 있는 객체  (Ctrl + shift + 'o') -> auto import
-    
-    public UserDAO() {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/BBS?serverTimezone=UTC";
+			String dbURL = "jdbc:mysql://localhost:3306/bbs?useUnicode=true&characterEncoding=UTF-8";
 			String dbID = "root";
 			String dbPassword = "root";
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  userID);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword)) {
+					return 1; // 로그인 성공
+				} else {
+					return 0; // 비밀번호 불일치
+				}
+			}
+			return -1; // 아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; // 데이터베이스 오류
+	}
     
-    public int login(String userID, String userPassword) {
-        String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+    public int join(User user) {
+        String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?)";
         try {
             pstmt = conn.prepareStatement(SQL);
-            pstmt.setString(1, userID);
-            rs = pstmt.executeQuery();
-            if(rs.next()){
-                if(rs.getString(1).equals(userPassword))
-                    return 1;    // 로그인 성공
-                else
-                    return 0; // 비밀번호 불일치
-            }
-            return -1; // ID가 없음
-            
-        } catch (Exception e) {
+            pstmt.setString(1, user.getUserID());
+            pstmt.setString(2, user.getUserPassword());
+            pstmt.setString(3, user.getUserName());
+            pstmt.setString(4, user.getUserGender());
+            pstmt.setString(5, user.getUserEmail());
+            return pstmt.executeUpdate();
+        } catch(Exception e) {
             e.printStackTrace();
         }
-        return -2; // DB 오류
-        
+        return -1; // 데이터베이스 오류
     }
  
 }
