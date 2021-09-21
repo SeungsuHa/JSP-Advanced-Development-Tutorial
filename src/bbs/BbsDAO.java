55722d1ad586ced8405d80c55c67089c2dede785
SeungSuHa <bonohot95@gmail.com>
2021-09-22 07:29:46 +0900

    게시글 목록,보기 기능

--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -1,6 +1,7 @@
 package bbs;
  
 import java.sql.*;
+import java.util.*;
  
 public class BbsDAO {
     private Connection conn;            // DB에 접근하는 객체
@@ -64,6 +65,68 @@ public class BbsDAO {
             e.printStackTrace();
         }
         return -1; // 데이터베이스 오류
-        
     }
-}
+    public ArrayList<Bbs> getList(int pageNumber)
+    {
+        String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10"; // 내림차순으로 가장 마지막에 쓰인 것을 가져온다
+        ArrayList<Bbs> list = new ArrayList<Bbs>();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1, getNext() - (pageNumber - 1 ) * 10);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Bbs bbs = new Bbs();
+                bbs.setBbsID(rs.getInt(1));
+                bbs.setBbsTitle(rs.getString(2));
+                bbs.setUserID(rs.getString(3));
+                bbs.setBbsDate(rs.getString(4));
+                bbs.setBbsContent(rs.getString(5));
+                bbs.setBbsAvailable(rs.getInt(6));
+                list.add(bbs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list; 
+    }
+//    페이징 처리를 위한 함수
+    public boolean nextPage(int pageNumber) {
+        String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1"; 
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1, getNext() - (pageNumber - 1 ) * 10);
+            rs = pstmt.executeQuery();
+            if (rs.next())
+            {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false; 
+    }
+    
+    public Bbs getBbs(int bbsID)
+    {
+            String SQL = "SELECT * FROM BBS WHERE bbsID = ?"; 
+            try {
+                PreparedStatement pstmt = conn.prepareStatement(SQL);
+                pstmt.setInt(1, bbsID);
+                rs = pstmt.executeQuery();
+                if (rs.next())
+                {
+                    Bbs bbs = new Bbs();
+                    bbs.setBbsID(rs.getInt(1));
+                    bbs.setBbsTitle(rs.getString(2));
+                    bbs.setUserID(rs.getString(3));
+                    bbs.setBbsDate(rs.getString(4));
+                    bbs.setBbsContent(rs.getString(5));
+                    bbs.setBbsAvailable(rs.getInt(6));
+                    return bbs;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null; 
+        }
+    }
